07c063e9c169af01b47033cf8c9ffa1c192c2a47
Benjamin Giese <bengiese22@gmail.com>
2020-10-27 19:31:03 -0600

    add a simple comment

--- src/main/java/fizzbuzz/FizzBuzz.java
+++ src/main/java/fizzbuzz/FizzBuzz.java
@@ -12,10 +12,13 @@ public class FizzBuzz {
 		String fizzBuzzStr = "";
 
 		for (int i = 1; i <= 20; i++) {
+
+			// If the number contains a "3" then the output should be "lucky", overwriting any other behavior
 			if (String.valueOf(i).contains("3")) {
 				fizzBuzzStr += "lucky ";
 			} else {
-				if ((i % 15) == 0) { // If it is a multiple of 15, then fizzbuzz. Needs to be before 3 and 5.
+				// Normal Behavior
+				if ((i % 15) == 0) { // If it is a multiple of 15, then fizzbuzz. Needs to be before 3 and 5
 					fizzBuzzStr += "fizzbuzz ";
 				} else if ((i % 3) == 0) { // If it is a multiple of 3, then fizz
 					fizzBuzzStr += "fizz ";
